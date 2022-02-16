d0f9d0396f9895d7a9a2d8bc6066db9c75fb8654
Your Name~ <naiumur202163@gmail.com>
2022-02-16 21:46:42 +0600

    user added

--- src/features/User.js
+++ src/features/User.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { UsersData } from "../FakeData";
 
 
 
 export const  userSlice=createSlice({
      name:'users',
-    initialState:{value:[]},
+    initialState:{value:UsersData},
     reducers:{
         addUser:(state,action)=>{
             // Addin a User
